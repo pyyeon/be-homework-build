01f2ac23d29fae32ac56132b9216d417eba352df
Jungmin Lee <75019459+Lucky-kor@users.noreply.github.com>
2024-06-06 21:35:52 +0900

    Add files via upload

---
+++ src/test/java/com/springboot/Section3Week3HomeworkBuildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section3Week3HomeworkBuildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
